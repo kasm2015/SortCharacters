ce4c993c561ad367e6e793b76bfa765516afc9ee
syekotwa <Yaghouse@786>
2020-04-05 20:07:38 +0530

    Read paragraph.txt file from resource folder and add the content to list

--- src/main/java/com/example/sortcharacter/SortCharacterApplication.java
+++ src/main/java/com/example/sortcharacter/SortCharacterApplication.java
@@ -3,11 +3,33 @@ package com.example.sortcharacter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 @SpringBootApplication
 public class SortCharacterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SortCharacterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SortCharacterApplication.class, args);
+        //Read a paragraph from a file located in resource folder
+        SortCharacterApplication.readFile();
+    }
+
+
+    private static void readFile() {
+        //reading text file into list
+        List<String> lines = Collections.EMPTY_LIST;
+        try {
+            lines = Files.readAllLines(Paths.get("src/main/resources/paragraph.txt"), StandardCharsets.UTF_8);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        System.out.println("Content of List:");
+        System.out.println(lines);
+    }
 
 }
