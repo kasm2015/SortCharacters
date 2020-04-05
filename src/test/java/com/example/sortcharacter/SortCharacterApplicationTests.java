453f7c75e85061191536308944fcd45896dd4601
syekotwa <Yaghouse@786>
2020-04-05 19:24:07 +0530

    Initial import

---
+++ src/test/java/com/example/sortcharacter/SortCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sortcharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortCharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
